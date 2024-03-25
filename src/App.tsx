1d0f8eaa5cba8c6b1b5a02cec7b2526d8f3647b5
NicuV23 <v.nicu213@gmail.com>
2024-03-25 17:54:37 +0200

    first repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+interface WeatherData {
+  name: string;
+  main: {
+    temp: number;
+  };
+  weather: {
+    main: string;
+    description: string;
+  }[];
+}
+
+const api = {
+  key: "6b5a7d894270a41854439e21cf573061",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
+  const [search, setSearch] = useState<string>("");
+  const [weather, setWeather] = useState<WeatherData | null>(null); // Specificarea tipului pentru weather
+
+  const searchPressed = () => {
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result: WeatherData) => {
+        setWeather(result);
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather App</h1>
+
+        <div>
+          <input
+            type="text"
+            placeholder="Enter city/town..."
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button onClick={searchPressed}>Search</button>
+        </div>
+
+        {weather && weather.main && weather.weather && (
+          <div>
+            <p>{weather.name}</p>
+
+            <p>{weather.main.temp}°C</p>
+
+            <p>{weather.weather[0].main}</p>
+            <p>({weather.weather[0].description})</p>
+          </div>
+        )}
       </header>
     </div>
   );
